ae53c18d3a068488ed83c0088ddebd7dd49df023
enzoxa <enzoxa@gmail.com>
2023-11-14 17:39:41 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'react-homework',
-  plugins: [react()],
+  plugins: [react()]
 })
 
